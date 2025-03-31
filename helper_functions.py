4e7aef4ddfd0254f58612e0c1787104564d1768b
Aaron Shackelford <grayjackel5@gmail.com>
2025-03-30 20:53:32 -0700

    adding debugging logic

--- helper_functions.py
+++ helper_functions.py
@@ -10,7 +10,7 @@ from decimal import Decimal
 
 
 VALIDATE_DATE = True
-
+DEBUGGING = False
 
 def is_minutes_apart(time1: time, time2: time, minutes: int = 30) -> bool:
     """
@@ -335,4 +335,7 @@ def invalid_date(day: date) -> bool:
     '''
     Returns True if the date is invalid, else returns false
     '''
+    if DEBUGGING:
+        return day <= date(year=2025,month=3,day=21) if VALIDATE_DATE else False
     return day <= last_friday() if VALIDATE_DATE else False
+
